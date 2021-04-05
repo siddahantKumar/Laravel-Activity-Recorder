29b3b99268bbab9886cd4e8b7ff543ba0578eeac
siddahantKumar <sidhantlohana@outlook.com>
2021-04-05 17:08:46 +0500

    Initial Commit

---
+++ app/User.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class User extends Authenticatable
+{
+    use Notifiable,LogsActivity; // through activitylog we can nootify the every user entry using tinker in app
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    // protected $fillable = [
+    //     'name', 'email', 'password',
+    // ];
+
+    protected $fillable = ['name', 'email']; // there are multiple advance events we can implement over here according to our need 
+
+
+    public function getDescriptionForEvent(string $eventName): string
+    {
+        return "you have  {$eventName} user" ;
+    }
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
